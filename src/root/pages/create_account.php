3a8c173af0ed81c95e4d04ae29c4f66f2e89bde8
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2022-09-26 22:18:43 -0400

    Add files via upload

--- src/root/pages/create_account.php
+++ src/root/pages/create_account.php
@@ -1,51 +1,64 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <title>Create Account</title>
-    </head>
+<head>
+    <title>Create Account</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
 
-    <body>
-        <h1>Create Your Account</h1>
-        <p>Enter your email, then create a username and password</p>
+<body>
+    <h1>Create Your Account</h1>
+    <p>Enter your email, then create a username and password. Then you will be redirected to the sign in page.</p>
 
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input class="form-control" name = "email" type="email" placeholder="Enter email" required />
-            </div>
-            <div class="form-group">
-                <label for="user">Username:</label>
-                <input class="form-control" name = "user" type="text" placeholder="Enter username" required/>
-            </div>
-            <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input class="form-control" name = "pwd" type="password" placeholder="Enter password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number, one uppercase, one lowercase letter, and 8 or more characters" required/>
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Create Account" name = "Submit">
-            </div>
-        </form>
-        <p>Already have an account? <a href="../index.php">sign in here!</a></p>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input class="form-control" name="email" type="email" placeholder="Enter email" required />
+        </div>
+        <div class="form-group">
+            <label for="user">Username:</label>
+            <input class="form-control" name="user" type="text" placeholder="Enter username" required />
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input class="form-control" name="pwd" type="password" placeholder="Enter password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number, one uppercase, one lowercase letter, and 8 or more characters" required />
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Create Account" name="Submit">
+        </div>
+    </form>
+    <p>Already have an account? <a href="../index.php">sign in here!</a></p>
 
-        <?php
-            require("../include_files/DBConn.php");
+    <?php
+    require("../include_files/DBConn.php");
+
+        if(isset($_POST['user']) && isset($_POST['pwd']) && isset($_POST['email'])){
 
             $username = $_POST['user'];
             $pass_word = $_POST['pwd'];
             $email = $_POST['email'];
 
-            try{
+            try {
                 $sth = $dbh->prepare('INSERT INTO user_info (username,pass_word,email) VALUES (?,?,?)');
                 $sth->execute(array($username, $pass_word, $email));
-                echo 'Account Created!';
-            }catch(PDOException $e){
-                exit("DB Err: ".$e->getMessage());
-                echo "An error has occurred. Please Try again later.";
+
+                #Uncomment line below when used on actual site and comment line with header after.
+                header("Location:https://pop-corn.azurewebsites.net/pages/profile_gen.php");
+
+                #Uncomment line below when using XAMPP during development
+                #header("Location:http://localhost/root/pages/profile_gen.php");
+
+            } catch (PDOException $e) {
+                echo "This user already exists! Try signing in..";
             }
 
-        ?>
+        }
+    ?>
 
-    </body>
+</body>
+
 </html>
---
+++ src/root/pages/profile_gen.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Select your preferred generes!</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+</head>
+
+
+<body>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#subBtn').click(function() {
+            checked = $("input[type=checkbox]:checked").length;
+
+            if(checked < 3) {
+                alert("You must check at least three checkbox.");
+                return false;
+            }
+
+            });
+        });
+    </script>
+
+    <h1>Choose your favorite genres of movies to watch</h1>
+    <form action="main_page.php">
+        <input type="checkbox" id="Action" name="Action" value="Action">
+        <label for="Action"> Action</label><br>
+
+        <input type="checkbox" id="Adventure" name="Adventure" value="Adventure">
+        <label for="Adventure"> Adventure</label><br>
+
+        <input type="checkbox" id="Animation" name="Animation" value="Animation">
+        <label for="Animation">Animation</label><br>
+
+        <input type="checkbox" id="Biography" name="Biography" value="Biography">
+        <label for="Biography">Biography</label><br>
+
+        <input type="checkbox" id="Comedy" name="Comedy" value="Comedy">
+        <label for="Comedy">Comedy</label><br>
+
+        <input type="checkbox" id="Crime" name="Crime" value="Crime">
+        <label for="Crime">Crime</label><br>
+
+        <input type="checkbox" id="Documentary" name="Documentary" value="Documentary">
+        <label for="Documentary">Documentary</label><br>
+
+        <input type="checkbox" id="Drama" name="Drama" value="Drama">
+        <label for="Drama">Drama</label><br>
+
+        <input type="checkbox" id="Family" name="Family" value="Family">
+        <label for="Family">Family</label><br>
+
+        <input type="checkbox" id="Fantasy" name="Fantasy" value="Fantasy">
+        <label for="Fantasy">Fantasy</label><br>
+        
+        <input type="checkbox" id="Film-Noir" name="Film-Noir" value="Film-Noir">
+        <label for="Film-Noir">Film-Noir</label><br>
+
+        <input type="checkbox" id="History" name="History" value="History">
+        <label for="History">History</label><br>
+
+        <input type="checkbox" id="Horror" name="Horror" value="Horror">
+        <label for="Horror">Horror</label><br>
+
+        <input type="checkbox" id="Music" name="Music" value="Music">
+        <label for="Music">Music</label><br>
+
+        <input type="checkbox" id="Musical" name="Musical" value="Musical">
+        <label for="Musical">Musical</label><br>
+
+        <input type="checkbox" id="Mystery" name="Mystery" value="Mystery">
+        <label for="Mystery">Mystery</label><br>
+
+        <input type="checkbox" id="Romance" name="Romance" value="Romance">
+        <label for="Romance">Romance</label><br>
+
+        <input type="checkbox" id="Sci-Fi" name="Sci-Fi" value="Sci-Fi">
+        <label for="Sci-Fi">Sci-Fi</label><br>
+
+        <input type="checkbox" id="Sport" name="Sport" value="Sport">
+        <label for="Sport">Sport</label><br>
+
+        <input type="checkbox" id="Thriller" name="Thriller" value="Thriller">
+        <label for="Thriller">Thriller</label><br>
+
+        <input type="checkbox" id="War" name="War" value="War">
+        <label for="War">War</label><br>
+
+        <input type="checkbox" id="Western" name="Western" value="Western">
+        <label for="Western">Western</label><br>
+
+        <input type="submit" id = "subBtn" value="Submit">
+    </form>
+</body>
+</html>
