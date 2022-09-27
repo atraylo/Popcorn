537aec2724402412e9ec8155c8879d9a9e9104ca
SytivaWheeler <69877161+SytivaWheeler@users.noreply.github.com>
2022-09-27 04:36:28 -0400

    Add files via upload

--- src/root/include_files/sign_in.php
+++ src/root/include_files/sign_in.php
@@ -1,36 +1,43 @@
 <?php
-    require("DBConn.php");
+session_start();
 
-    if(isset($_POST['user']) && isset($_POST['pwd'])){
+require("DBConn.php");
+
+
+if (isset($_POST['user']) && isset($_POST['pwd'])) {
+
+    #User entered data
+    $username = $_POST['user'];
+    $pass_word = $_POST['pwd'];    
+
+    #Prep sql statments
+    $sth = $dbh->prepare('SELECT * FROM user_info WHERE username = ?');
+    $sth->execute(array($username));
+
+    $usercheck = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+    #Check if username exists in database
+    if ($usercheck[0]['username'] == $username) {
+
+        #Check if password is correct
+        if ($usercheck[0]['pass_word'] == $pass_word) {
+
+            #Sign in
+
+            #Uncomment line below when used on actual site and comment line with header after.
             
-        #User entered data
-        $username = $_POST['user'];
-        $pass_word = $_POST['pwd'];
-
-        #Prep sql statments
-        $sth = $dbh->prepare('SELECT * FROM user_info WHERE username = ?');
-        $sth->execute(array($username));
-
-        $usercheck = $sth->fetchAll(\PDO::FETCH_ASSOC);
-
-        #Check if username exists in database
-        if ($usercheck[0]['username'] == $username) {
-
-            #Check if password is correct
-            if($usercheck[0]['pass_word'] == $pass_word){
-                
-                #Sign in
-
-                #Uncomment line below when used on actual site and comment line with header after.
-                header("Location:https://pop-corn.azurewebsites.net/pages/main_page.php");
-
-                #Uncomment line below when using XAMPP during development
-                #header("Location:http://localhost/root/pages/main_page.php");
-            } else {
-                echo "<td> Your passsword is incorrect.. </td>";
-            }
+            header("Location:https://pop-corn.azurewebsites.net/pages/main_page.php");
+            
+            #Uncomment line below when using XAMPP during development
+            #header("Location:http://localhost/root/pages/main_page.php");
         } else {
-            echo "<td> That username does not exist.. </td>";
+            $em = "Your passsword is incorrect..";
+
+            header("Location:../index.php?error=$em");    
         }
+    } else {
+        $em = "That username does not exist..";
+
+        header("Location:../index.php?error=$em");      
     }
-?>
+}
